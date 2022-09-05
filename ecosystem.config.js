b165d8450ee0377c8c5ff41eedbdc9984692d920
urielch <urielch@gmail.com>
2022-09-05 11:33:54 +0200

    use /local as default prefix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,7 @@ module.exports = {
             max_memory_restart: '1G',
             env: {
                 NODE_ENV: 'production',
+                GLOBAL_PREFIX: '/local',
             },
         },
     ],
