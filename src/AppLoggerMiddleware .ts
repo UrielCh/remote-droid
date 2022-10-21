207e745ef89d17fff9484a6dae4997388e2ed491
UrielCh <uriel.chemouni@gmail.com>
2022-10-21 13:16:53 +0300

    add live in cnt

--- src/AppLoggerMiddleware .ts
+++ src/AppLoggerMiddleware .ts
@@ -4,21 +4,25 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class AppLoggerMiddleware implements NestMiddleware {
+  nbOpen = 0;
+  nbClose = 0;
   private logger = new Logger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
+    this.nbOpen++;
     const start = Date.now();
     const { method, originalUrl: url } = request;
     let { ip } = request;
     ip = ip.replace('::ffff:', '');
 
     let timer: NodeJS.Timeout | null = setTimeout(() => {
-      this.logger.log(`${ip.padEnd(11)} ${method} ${url} - TIMEOUT`);
+      const live = this.nbOpen - this.nbClose;
+      this.logger.log(`${ip.padEnd(11)} ${method} ${url} - TIMEOUT live:${live}`);
       timer = null;
     }, 10000);
 
     response.on('close', () => {
-      const dur = Date.now() - start;
+      this.nbClose++;
       const { statusCode } = response;
       // const contentLength = response.get('content-length');
       if (timer) {
@@ -26,10 +30,11 @@ export class AppLoggerMiddleware implements NestMiddleware {
         clearTimeout(timer);
         timer = null;
       } else {
-        this.logger.log(`${ip.padEnd(11)} ${method} ${url} - ${statusCode} -${dur}ms After timeout`);
+        const dur = Date.now() - start;
+        const live = this.nbOpen - this.nbClose;
+        this.logger.log(`${ip.padEnd(11)} ${method} ${url} - ${statusCode} -${dur}ms After timeout live:${live}`);
       }
     });
-
     next();
   }
 }
