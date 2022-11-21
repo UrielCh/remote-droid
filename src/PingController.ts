1622743aa167ec39418c0b223c5a31296a37c69b
UrielCh <uriel.chemouni@gmail.com>
2022-11-21 16:13:05 +0200

    rename /ping/apoptose to /apoptose

--- src/PingController.ts
+++ src/PingController.ts
@@ -7,26 +7,38 @@ import { DroidUserFull } from './db/user.entity';
 
 @Injectable()
 @ApiTags('Info')
-@Controller('/ping')
+@Controller('/')
 export class PingController {
   @ApiOperation({
     summary: 'ping',
     description: 'ping entry point replay pong',
   })
-  @Get('/')
+  @Get('/ping')
   getPing(): string {
     return 'pong';
   }
 
   @ApiOperation({
-    summary: 'apoptose the POD',
+    summary: 'Apoptosis, apoptose the POD',
     description: 'cause the server to shutdown',
   })
   @Get('/apoptose')
   @ApiBearerAuth('BearerToken')
   @UseGuards(TokenGuard)
   Apoptose(@GetUser() user: DroidUserFull): string {
-    console.log('Apoptose called');
+    if (!user.allowDevice('*')) throw new UnauthorizedError();
+    setTimeout(() => process.exit(0), 100);
+    return 'will reboot in 100 ms';
+  }
+
+  @ApiOperation({
+    summary: 'Apoptosis, apoptose the POD [deprecated]',
+    description: 'cause the server to shutdown',
+  })
+  @Get('/ping/apoptose')
+  @ApiBearerAuth('BearerToken')
+  @UseGuards(TokenGuard)
+  ApoptoseOld(@GetUser() user: DroidUserFull): string {
     if (!user.allowDevice('*')) throw new UnauthorizedError();
     setTimeout(() => process.exit(0), 100);
     return 'will reboot in 100 ms';
