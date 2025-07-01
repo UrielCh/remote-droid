2781095b2c121146d4ee176ef5dd308943c9ef97
urielch <uriel.chemouni@gmail.com>
2025-07-01 13:45:32 +0200

    feat: add static file serving with @nestjs/serve-static and basic index.html

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module.js';
@@ -7,9 +9,25 @@ import { DBModule } from './db/db.module.js';
 import { InfoModule } from './info/info.module.js';
 import { PingController } from './PingController.js';
 import { HealthModule } from './health/health.module.js';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({ isGlobal: true }), DeviceModule, DBModule, InfoModule, UserModule, HealthModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // This tells NestJS where to find your frontend files
+      // serveRoot: '/frontend', // Uncomment this if you want to serve the frontend from a specific sub-path, e.g., http://localhost:3000/frontend
+    }),
+    AuthModule, 
+    ConfigModule.forRoot({ isGlobal: true }), 
+    DeviceModule, 
+    DBModule, 
+    InfoModule, 
+    UserModule, 
+    HealthModule
+  ],
   controllers: [PingController],
   providers: [],
 })
